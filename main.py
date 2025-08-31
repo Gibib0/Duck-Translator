1cdd269d5d2e0df422eb85cb8ff89d34e0ff18bd
Anastasia <anast111antonova@gmail.com>
2025-09-01 01:48:13 +0300

    Fix bugs; add welcome message

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ class DuckTranslator:
 		mixer.init()
 		
 		mixer.music.load('assets/sounds/background_sound.mp3')
+		mixer.music.set_volume(0.3)
+
 		self.duck_sound = mixer.Sound('assets/sounds/duck_sound.mp3')
 		self.holy_sound = mixer.Sound('assets/sounds/holy_sound.mp3')
 		self.screamer_sound = mixer.Sound('assets/sounds/screamer_sound.mp3')
@@ -51,22 +53,28 @@ class DuckTranslator:
 		# DuckCounter
 
 		self.duck_count = 0
-		self.counter_running = True
+		self.counter_running = False
 		self.scarer_shown = False
 
 		# InteractiveElements
 
 		self.create_widgets()
+
+		self.show_welcome_message()
+		
 		self.play_music()
 		self.duck_counter()
 
+
+	def show_welcome_message(self):
+		messagebox.showwarning('From this moment, ducks will come to you every second. (Don`t wait until 300...)\n Press OK to start.')
 
 	def create_widgets(self):
 		label = tk.Label(self.root, text = 'DUCK Translator',
 										font=('Comic Sans', 20, 'bold'), bg='white', relief='raised')
 		label.pack(pady=10)
 
-		self.text_input = tk.Entry(self.root, width=50, font=('Comic Sans', 12))
+		self.text_input = tk.Entry(self.root, width=50, height=4, bg='pink', font=('Comic Sans', 12))
 		self.text_input.pack(pady=5)
 		self.text_input.insert(0, 'Enter your text here.....')
 		self.text_input.bind('<FocusIn>', self.clear_placeholder)
@@ -74,9 +82,9 @@ class DuckTranslator:
 		translate_btn = tk.Button(self.root, text='Translate to Duck',
 															command=self.translate_text, bg='yellow', 
 															font=('Comic Sans', 10))
-		translate_btn.pack(pady=5)
+		translate_btn.pack(pady=10)
 
-		self.text_output = tk.Text(self.root, width=50, height=6, font=('Comic Sans', 10))
+		self.text_output = tk.Text(self.root, width=50, height=4, bg='pink', font=('Comic Sans', 10))
 		self.text_output.pack(pady=10)
 
 		praise_btn = tk.Button(self.root, text='Praise the Duck',
@@ -84,7 +92,7 @@ class DuckTranslator:
 													font=('Comic Sans', 12, 'bold'))
 		praise_btn.pack(pady=10)
 
-		self.counter_label = tk.Label(self.root, text='0 🦆',
+		self.counter_label = tk.Label(self.root, text='Ducks arrived: 0 🦆',
 																font=('Comic Sans', 12), bg='lightgrey')
 		self.counter_label.pack(side=tk.BOTTOM, pady=10)
 
@@ -103,6 +111,7 @@ class DuckTranslator:
 		mixer.music.unpause()
 
 	def duck_counter(self):
+		self.counter_running = True
 		def counter_thread():
 			while self.counter_running:
 				time.sleep(1)
@@ -115,9 +124,27 @@ class DuckTranslator:
 	def update_counter_label(self):
 		self.counter_label.config(text=f'{self.duck_count} 🦆')
 
-		if self.duck_count == 100 and not self.scarer_shown:
+		if self.duck_count == 300 and not self.scarer_shown:
 			self.screamer = True
 			self.screamer()
+
+	def screamer(self):
+		self.pause_music()
+
+		screamer_window = tk.Toplevel(self.root)
+		screamer_window.geometry('400x400')
+		screamer_window.attributes('-topmost', True)
+
+		label = tk.Label(screamer_window, image = self.screamer_photo)
+		label.pack()
+
+		self.screamer_sound.play()
+
+		self.root.after(1000, lambda: self.close_screamer(screamer_window))
+
+	def close_screamer(self, window):
+		window.destroy()
+		self.resume_music()
 
 	def translate_text(self):
 		self.duck_sound()
@@ -135,25 +162,6 @@ class DuckTranslator:
 		self.text_output.delete(1.0, tk.END)
 		self.text_output.insert(tk.END, duck_translation)
 
-	def screamer(self):
-		self.pause_music()
-
-		screamer_window = tk.Toplevel(self.root)
-		screamer_window.geometry('400x400')
-		screamer_window.attributes('-topmost', True)
-
-		label = tk.Label(screamer_window, image = self.screamer_photo)
-		label.pack()
-
-		self.screamer_sound.play()
-
-		self.root.after(1000, lambda: self.close_screamer(screamer_window))
-
-	def on_closing(self):
-		"""Действие при закрытии окна."""
-		self.counter_running = False
-		self.root.destroy()
-
 	def show_love(self):
 		self.pause_music()
 		self.ILoveU_sound.play()
@@ -164,10 +172,10 @@ class DuckTranslator:
 		label = tk.Label(love_window, image=self.romantic_photo)
 		label.pack(pady=10)
 
-		close_btn = tk.Button(love_window, text='OK', command=lambda: self.clode_love_window(love_window), bg = 'pink', font=('Comic Sans', 12))
+		close_btn = tk.Button(love_window, text='OK', command=lambda: self.close_love_window(love_window), bg = 'pink', font=('Comic Sans', 12))
 		close_btn.pack(pady=10)
 
-	def clode_love_window(self, window):
+	def close_love_window(self, window):
 		window.destroy()
 		messagebox.showinfo('And I love u /ᐠ｡ꞈ｡ᐟ\ ')
 		self.resume_music()
@@ -186,9 +194,34 @@ class DuckTranslator:
 		close_btn = tk.Label(praise_window, text='OK', command=lambda: self.slode_praise_window(praise_window), bg='skyblue', font=('Comic Sans', 12))
 		close_btn.pack(pady=10)
 
-	def close_praise_window(self, window):
+		next_btn = tk.Button(praise_window, text='OK', command=lambda: self.show_holy_duck(praise_window), bg='lightgreen', font=('Comic Sans', 12))
+		next_btn.pack(pady=10)
+
+	def show_holy_duck(self, first_window):
+		first_window.destroy()
+
+		holy_window = tk.Toplevel(self.root)
+		holy_window.geometry('350x400')
+
+		label = tk.Label(holy_window, image=self.duck_god_photo)
+		label.pack(pady=20)
+
+		text_label = tk.Label(holy_window, text='Blessed are those who praise the Duck~!', font=('Comic Sans', 12, 'bold'))
+		text_label.pack(pady=10)
+
+		close_btn = tk.Button(holy_window, text='Amen', command=lambda: self.close_holy_window(holy_window), bg='gold', font=('Comic Sans', 12))
+
+		close_btn.pack(pady=10)
+
+		self.holy_sound.play()
+
+	def close_holy_window(self, window):
 		window.destroy()
 		self.resume_music()
+
+		def on_closing(self):
+			self.counter_running = False
+			self.root.destroy()
 
 if __name__ == '__main__':
 	root = tk.Tk()
